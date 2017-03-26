5ec083900af8c4398948e0f65c82ff7bfaa93e7b
James Costello <jcostello10@gmail.com>
2017-03-26 13:13:28 -0700

    sigma

--- stats.py
+++ stats.py
@@ -20,3 +20,15 @@
 # The Median is less susceptible to outliers
 # Mode is most common value, only works for discrete values
 
+# Standard Deviation & Variance
+
+# Two fundamental qualities for the spread and shape of data
+#The shape of a histogram can tell you a lot about the distribution
+
+# Variance is simply the average of the squared differences from the mean
+
+# Example what is the variance of the data set (1,4,5,4,8)?
+# First find the mean (1+4+5+4+8)/5 = 4.4
+# Now the differene (-3.4, -0.4, 0.6, -0.4, 3.6)
+# Square them (11.56,0.16,0.36,0.16,12.96)
+# sigma = (11.56,0.16,0.36,0.16,12.96)/5 = 5.04
