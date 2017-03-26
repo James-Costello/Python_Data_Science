681c8f644a2c4a2d2af83bcb9d71e9fc68cb70b8
James Costello <jcostello10@gmail.com>
2017-03-25 17:01:51 -0700

    Initialize Repo

---
+++ stats.py
@@ -0,0 +1,22 @@
+# Types of Data
+
+# Numerical
+# Quantifiable
+# Discrete Values and Continous Data
+
+# Categorical
+# No inherent mathematical meaning
+# Gender, yes/no, race, state of residence, political party
+# numbers have no mathematical relationship
+
+# Ordinal
+# A mixture of Numerical and Categorical
+# So movie ratings
+# measure of quality
+
+# Review
+# Mean, Median, Mode
+
+# The Median is less susceptible to outliers
+# Mode is most common value, only works for discrete values
+
