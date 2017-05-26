37f5c49733173449644aa93f23bfaf20bb1e0a89
James Costello <jcostello10@gmail.com>
2017-05-26 12:22:43 -0700

    pandas

--- numpy_notes.py
+++ numpy_notes.py
@@ -7,3 +7,11 @@ np.load('myarray.npy')
 # npy is the file extension
 
 np.savez('ziparray.npz', x=arr1, y=arr2)
+# zip file extensions with multiple arrays
+
+archive_array = np.load('ziparray.npz')
+archive_array['x']
+
+np.savetxt('mytextarray.txt', arr, delimiter=',')
+
+arr = np.loadtxt('mytextarray.txt', delimiter=',')
